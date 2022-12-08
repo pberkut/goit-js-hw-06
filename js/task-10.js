6511be5034c479314da2a1d1ff602c26c462365d
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-08 22:44:09 +0200

    Completed Task # 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnCreateRef = document.querySelector('button[data-create]');
+const btnDestroyRef = document.querySelector('button[data-destroy]');
+const inputRef = document.querySelector('input[type=number]');
+const boxesRef = document.querySelector('#boxes');
+
+btnCreateRef.addEventListener('click', () => {
+  const amount = inputRef.value;
+  if (amount === '') {
+    alert('Введи число');
+  }
+  createBoxes(amount);
+});
+
+btnDestroyRef.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  for (let i = 1, x = 0; i <= amount; i += 1, x += 10) {
+    boxesRef.insertAdjacentHTML(
+      'beforeend',
+      `<div style="background-color: ${getRandomHexColor()}; width: ${30 + x}px; height: ${
+        30 + x
+      }px"> </div>`,
+    );
+  }
+}
+
+function destroyBoxes() {
+  boxesRef.innerHTML = '';
+}
