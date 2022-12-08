49c40f0928430afccdc604cd47b61e5891627553
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-08 21:30:23 +0200

    Completed Task # 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+const spanColorValue = document.querySelector('.color');
+
+btnRef.addEventListener('click', handleBtnChangeColor);
+
+function handleBtnChangeColor(event) {
+  const randomColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = randomColor;
+  spanColorValue.textContent = randomColor;
+  console.log(randomColor);
+}
