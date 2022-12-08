81d22356a1109ca8d2476da05c196b1cc1605711
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-08 20:38:50 +0200

    Complete Task # 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,8 @@
 const fontSizeControlRef = document.querySelector('#font-size-control');
+const textFontSizeRef = document.querySelector('#text');
+
+fontSizeControlRef.addEventListener('input', isChangeFontSize);
+
+function isChangeFontSize(event) {
+  textFontSizeRef.style.fontSize = `${event.target.value}px`;
+}
