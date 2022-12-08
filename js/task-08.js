46ee0f4e203d14add943c5446a944aded4ec89c6
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-08 21:16:44 +0200

    Complete Task # 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const loginFormRef = document.querySelector('.login-form');
 
+loginFormRef.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === '' && password.value === '') {
+    return alert('Все поля должны быть заполнены.');
+  }
+  console.log({ email: email.value, password: password.value });
+  loginFormRef.reset();
+}
